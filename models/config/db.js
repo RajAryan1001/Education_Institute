6a7c8dca4168b865e4fdc93268223396a5fbd865
rajaryan7803 <rajaryan7803@gmail.com>
2025-04-19 17:20:48 +0530

    cluster setup

--- models/config/db.js
+++ models/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Education').then(()=>{
+mongoose.connect('mongodb+srv://Raj:Rajaryan@cluster0.crfzpz6.mongodb.net/').then(()=>{
     console.log('connected');
 }).catch((err)=>{
     console.log(err);

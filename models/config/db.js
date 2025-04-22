e99f49ba8e537833bec18c7a95b0111cd36090ed
rajaryan7803 <rajaryan7803@gmail.com>
2025-04-22 19:11:29 +0530

    we

--- models/config/db.js
+++ models/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Raj:Rajaryan@cluster0.crfzpz6.mongodb.net/').then(()=>{
+mongoose.connect('mongodb+srv://Raj:Rajaryan@cluster0.crfzpz6.mongodb.net/education').then(()=>{
     
     console.log('connected');
 }).catch((err)=>{
